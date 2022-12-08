e0930bac1c58747f2647cbc691ba0debd736fdd5
Hange777 <1921487592@qq.com>
2022-12-08 23:53:05 +0800

    update submodule

--- GIMBAL/COMMUNICATION/can2_send.c
+++ GIMBAL/COMMUNICATION/can2_send.c
@@ -13,18 +13,17 @@ void can2_gimbal_to_chassis(void)
 {
 	uint32_t send_mail_box; //发送邮箱
 	uint8_t Data[5];		//发送数据的数组
-	
-	
+
 	Txmessage.StdId = 0x300;	  //根据820r设置标识符
 	Txmessage.IDE = CAN_ID_STD;	  //指定将要传输的消息的标识符的类型
 	Txmessage.RTR = CAN_RTR_DATA; //指定的帧将被传输的消息的类型   数据帧或远程帧
 	Txmessage.DLC = 4;
-	
-	__asm__ 
-	(
-		"LDR R5 , [&Gimbal_Control.chassis_gimbal_angel] \n"
-		"STR R5 , [Data] \n"
-	);
-	
+
+__asm__ volatile
+{
+	LDR R5 , [&Gimbal_Control.chassis_gimbal_angel]
+	STR R5 , [Data]
+}
+
 	HAL_CAN_AddTxMessage(&hcan2, &Txmessage, Data, &send_mail_box); //将一段数据通过 CAN 总线发送
 }
